1b385cd0185bd41e5e34a954b03f041689e339de
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-09-02 18:22:10 +0800

    完成整合VueJS與SpringMVC
    1.加入靜態資源訪問設定
    2.執行npm run build

--- src/main/java/_00_Config/WebConfig.java
+++ src/main/java/_00_Config/WebConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -15,7 +17,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ctbc.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -23,30 +25,39 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 將 /static/ 打頭的請求，告訴SpringMVC的DispatcherServlet，幫忙mapping到/WEB-INF/static/目錄下
+		registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
+	}
+
 	// (1)
 	// 自定義SpringMVC的時間日期轉換器，當@RequestMapping 的方法 return 物件 (@ReqponseBody) 時，
 	// 若沒以下自訂時間日期轉換器，時間日期會變成中文，ex : 十一月 17, 1981
 	// 【參考】: https://www.jianshu.com/p/7211dfdbbb9d
 	@Bean
 	public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
-	    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-	    com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
-	    mapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	    mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-	    converter.setObjectMapper(mapper);
-	    return converter;
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+		mapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+		converter.setObjectMapper(mapper);
+		return converter;
 	}
-	
+
 	// (2)
 	// 將自訂時間日期轉換器加入SpringMVC 的轉換器列表中
 	@Override
 	public void configureMessageConverters(java.util.List<HttpMessageConverter<?>> converters) {
-	    // 将我们定义的时间格式转换器添加到转换器列表中,
-	    // 這樣jackson格式化时候但凡遇到 Date 类型就会转换成我们定义的格式
-	    converters.add(this.jackson2HttpMessageConverter());
+		// 将我们定义的时间格式转换器添加到转换器列表中,
+		// 這樣jackson格式化时候但凡遇到 Date 类型就会转换成我们定义的格式
+		converters.add(this.jackson2HttpMessageConverter());
 	}
 
 }
-
-
---
+++ src/main/webapp/indexVue.jsp
@@ -0,0 +1,38 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>vue_spring_mvc_test</title>
+    <link rel=stylesheet href=https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css>
+    <link rel=stylesheet href="<%=request.getContextPath()%>/static/assest/css/style4.css">
+    <script defer=defer src=https://use.fontawesome.com/releases/v5.0.13/js/solid.js></script>
+    <script defer=defer src=https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js></script>
+    <link rel="stylesheet" href="<%=request.getContextPath()%>/static/css/app.871fdee4a2afe455cde6ce6b2a94b21d.css">
+  </head>
+   <body>
+   
+      <div id=app></div>
+      
+      <script src=https://code.jquery.com/jquery-3.3.1.min.js></script>
+      <script src=https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js></script>
+      <script src=https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js></script>
+      <script type=text/javascript>
+      	$(document).ready(function () {
+      		$('#sidebarCollapse').on('click', function () {
+         		$('#sidebar').toggleClass('active'); 
+         	});
+      	});
+      </script> 
+      
+      <script type=text/javascript src="<%=request.getContextPath()%>/static/js/manifest.2ae2e69a05c33dfc65f8.js"></script>
+      <script type=text/javascript src="<%=request.getContextPath()%>/static/js/vendor.3d1fc959ab9251795ea9.js"></script> 
+      <script type=text/javascript src="<%=request.getContextPath()%>/static/js/app.0a53dc925298072ee7a5.js"></script>
+   </body>
+</html>
+
+
+
+
